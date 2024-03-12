5f3d349890347d6f7891d73e1bfef7fc47ea00c9
ally2211 <amendieta.va@gmail.com>
2024-03-11 23:25:35 -0500

    add number to route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Import the Flask module"""
+from flask import Flask
+
+""" Create an instance of the Flask class"""
+app = Flask(__name__)
+
+
+"""set strict slashes to false"""
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    # Replace underscores with spaces in the text
+    text_with_spaces = text.replace("_", " ")
+    return "C " + text_with_spaces
+
+
+@app.route('/python/<text>')
+def python_is_magic(text='is cool'):
+    # Replace underscores with spaces in the text
+    text_with_spaces = text.replace("_", " ")
+    return "Python " + text_with_spaces
+
+
+@app.route('/number/<int:n>')
+def show_number(n):
+    return '%d is a number' % n
+
+
+""" The server only starts if the script is executed directly"""
+if __name__ == "__main__":
+    # Run the Flask application on host '0.0.0.0' and port '5000'
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
